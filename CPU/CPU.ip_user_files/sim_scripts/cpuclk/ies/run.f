fae2b1ade02c8753df9dcf3922bdeafa76d33107
zhuyucheng30428 <tony30428@gmail.com>
2023-05-25 14:09:14 +0800

    tom

--- CPU/CPU.ip_user_files/sim_scripts/cpuclk/ies/run.f
+++ CPU/CPU.ip_user_files/sim_scripts/cpuclk/ies/run.f
@@ -1,9 +1,3 @@
--makelib ies_lib/xil_defaultlib -sv \
-  "C:/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
--endlib
--makelib ies_lib/xpm \
-  "C:/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_VCOMP.vhd" \
--endlib
 -makelib ies_lib/xil_defaultlib \
   "../../../../../cpuclk/cpuclk_clk_wiz.v" \
   "../../../../../cpuclk/cpuclk.v" \
