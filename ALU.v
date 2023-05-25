e8e232fb6733e4566236000d48eb7b897a4adebb
zhuyucheng30428 <tony30428@gmail.com>
2023-05-25 14:33:36 +0800

    ok

--- ALU.v
+++ ALU.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
 `include "definitions.v"
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/05/21 20:08:47
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ALU(Read_data_1,Read_data_2,Sign_extend,Function_opcode,Exe_opcode,ALUOp,
                  Shamt,ALUSrc,I_format,Zero,Jr,Sftmd,ALU_Result,Addr_Result,PC_plus_4
--- Data_Mem.v
+++ Data_Mem.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
 `include "definitions.v"
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/05/21 20:08:47
-// Design Name: 
-// Module Name: Data_mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Data_mem(clock, memWrite, address, writeData, readData);
     input clock;   //Clock signal.
