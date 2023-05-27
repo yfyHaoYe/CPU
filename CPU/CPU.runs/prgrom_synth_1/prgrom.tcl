0d52736679902742b7a1ae8188752dcc7a11cff5
yfyHaoYe <12112601@ustech.edu.cnhttps://github.com/yfyHaoYe/CPU.git>
2023-05-27 22:15:26 +0800

    123

--- CPU/CPU.runs/prgrom_synth_1/prgrom.tcl
+++ CPU/CPU.runs/prgrom_synth_1/prgrom.tcl
@@ -31,8 +31,8 @@ set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property ip_output_repo {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_ip -quiet {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom.xci}}
-set_property used_in_implementation false [get_files -all {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_ooc.xdc}}]
+read_ip -quiet {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom.xci}}
+set_property used_in_implementation false [get_files -all {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_ooc.xdc}}]
 
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -84,32 +84,32 @@ write_checkpoint -force -noxdef prgrom.dcp
 create_report "prgrom_synth_1_synth_report_utilization_0" "report_utilization -file prgrom_utilization_synth.rpt -pb prgrom_utilization_synth.pb"
 
 if { [catch {
-  file copy -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom.dcp} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom.dcp}
+  file copy -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom.dcp} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom.dcp}
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_stub.v}
+  write_verilog -force -mode synth_stub {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_stub.v}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_stub.vhdl}
+  write_vhdl -force -mode synth_stub {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_stub.vhdl}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_sim_netlist.v}
+  write_verilog -force -mode funcsim {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_sim_netlist.v}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_sim_netlist.vhdl}
+  write_vhdl -force -mode funcsim {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_sim_netlist.vhdl}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -119,32 +119,32 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom.dcp} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom.dcp}
+  file copy -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom.dcp} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom.dcp}
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_stub.v} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_stub.v}
+  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_stub.v} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_stub.v}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_stub.vhdl} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_stub.vhdl}
+  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_stub.vhdl} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_stub.vhdl}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_sim_netlist.v} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_sim_netlist.v}
+  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_sim_netlist.v} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_sim_netlist.v}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_sim_netlist.vhdl} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_sim_netlist.vhdl}
+  file rename -force {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/prgrom_synth_1/prgrom_sim_netlist.vhdl} {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_sim_netlist.vhdl}
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -153,12 +153,12 @@ if { [catch {
 
 if {[file isdir {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.ip_user_files/ip/prgrom}]} {
   catch { 
-    file copy -force {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_stub.v}} {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.ip_user_files/ip/prgrom}
+    file copy -force {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_stub.v}} {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.ip_user_files/ip/prgrom}
   }
 }
 
 if {[file isdir {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.ip_user_files/ip/prgrom}]} {
   catch { 
-    file copy -force {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.srcs/sources_1/ip/prgrom_1/prgrom_stub.vhdl}} {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.ip_user_files/ip/prgrom}
+    file copy -force {{c:/Users/29266/Desktop/Homework/Computer Organization/CPU/prgrom/prgrom_stub.vhdl}} {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.ip_user_files/ip/prgrom}
   }
 }
--- RAM/RAM.veo
+++ RAM/RAM.veo
@@ -57,7 +57,7 @@
 RAM your_instance_name (
   .clka(clka),    // input wire clka
   .wea(wea),      // input wire [0 : 0] wea
-  .addra(addra),  // input wire [13 : 0] addra
+  .addra(addra),  // input wire [14 : 0] addra
   .dina(dina),    // input wire [31 : 0] dina
   .douta(douta)  // output wire [31 : 0] douta
 );
