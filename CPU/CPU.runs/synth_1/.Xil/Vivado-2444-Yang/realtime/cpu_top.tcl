88957eed5d9fc6ca0bc1806a34a08b9ccabc678c
yfyHaoYe <12112601@ustech.edu.cnhttps://github.com/yfyHaoYe/CPU.git>
2023-05-27 22:30:33 +0800

    1111

--- CPU/CPU.runs/synth_1/.Xil/Vivado-2444-Yang/realtime/cpu_top.tcl
+++ CPU/CPU.runs/synth_1/.Xil/Vivado-2444-Yang/realtime/cpu_top.tcl
@@ -9,29 +9,25 @@ set rt::rc [catch {
   uplevel #0 {
     set ::env(BUILTIN_SYNTH) true
     source $::env(HRT_TCL_PATH)/rtSynthPrep.tcl
-    rt::HARTNDb_resetJobStats
-    rt::HARTNDb_resetSystemStats
-    rt::HARTNDb_startSystemStats
     rt::HARTNDb_startJobStats
     set rt::cmdEcho 0
     rt::set_parameter writeXmsg true
     rt::set_parameter enableParallelHelperSpawn true
     set ::env(RT_TMP) "C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/synth_1/.Xil/Vivado-2444-Yang/realtime/tmp"
     if { [ info exists ::env(RT_TMP) ] } {
-      file delete -force $::env(RT_TMP)
       file mkdir $::env(RT_TMP)
     }
 
     rt::delete_design
 
     set rt::partid xc7a35tcsg324-1
-    source $::env(HRT_TCL_PATH)/rtSynthParallelPrep.tcl
 
     set rt::multiChipSynthesisFlow false
     source $::env(SYNTH_COMMON)/common_vhdl.tcl
     set rt::defaultWorkLibName xil_defaultlib
 
-    set rt::useElabCache false
+    # Skipping read_* RTL commands because this is post-elab optimize flow
+    set rt::useElabCache true
     if {$rt::useElabCache == false} {
       rt::read_verilog -sv -include {
     {c:/Users/29266/Desktop/Homework/Computer Organization/CPU/cpuclk}
@@ -62,18 +58,19 @@ set rt::rc [catch {
       rt::read_vhdl -lib xpm C:/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_VCOMP.vhd
       rt::filesetChecksum
     }
-    rt::set_parameter usePostFindUniquification false
+    rt::set_parameter usePostFindUniquification true
+    set rt::SDCFileList {{C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/synth_1/.Xil/Vivado-2444-Yang/realtime/cpu_top_synth.xdc}}
+    rt::sdcChecksum
     set rt::top cpu_top
     set rt::reportTiming false
-    rt::set_parameter elaborateOnly true
-    rt::set_parameter elaborateRtl true
-    rt::set_parameter eliminateRedundantBitOperator false
+    rt::set_parameter elaborateOnly false
+    rt::set_parameter elaborateRtl false
+    rt::set_parameter eliminateRedundantBitOperator true
     rt::set_parameter writeBlackboxInterface true
+    rt::set_parameter ramStyle auto
     rt::set_parameter merge_flipflops true
-    rt::set_parameter srlDepthThreshold 3
-    rt::set_parameter rstSrlDepthThreshold 4
 # MODE: 
-    rt::set_parameter webTalkPath {}
+    rt::set_parameter webTalkPath {C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.cache/wt}
     rt::set_parameter enableSplitFlowPath "C:/Users/29266/Desktop/Homework/Computer Organization/CPU/CPU/CPU.runs/synth_1/.Xil/Vivado-2444-Yang/"
     set ok_to_delete_rt_tmp true 
     if { [rt::get_parameter parallelDebug] } { 
@@ -82,16 +79,23 @@ set rt::rc [catch {
     if {$rt::useElabCache == false} {
         set oldMIITMVal [rt::get_parameter maxInputIncreaseToMerge]; rt::set_parameter maxInputIncreaseToMerge 1000
         set oldCDPCRL [rt::get_parameter createDfgPartConstrRecurLimit]; rt::set_parameter createDfgPartConstrRecurLimit 1
-      rt::run_rtlelab -module $rt::top
+      rt::run_synthesis -module $rt::top
         rt::set_parameter maxInputIncreaseToMerge $oldMIITMVal
         rt::set_parameter createDfgPartConstrRecurLimit $oldCDPCRL
     }
 
     set rt::flowresult [ source $::env(SYNTH_COMMON)/flow.tcl ]
     rt::HARTNDb_stopJobStats
+    rt::HARTNDb_reportJobStats "Synthesis Optimization Runtime"
+    rt::HARTNDb_stopSystemStats
     if { $rt::flowresult == 1 } { return -code error }
 
 
+  set hsKey [rt::get_parameter helper_shm_key] 
+  if { $hsKey != "" && [info exists ::env(BUILTIN_SYNTH)] && [rt::get_parameter enableParallelHelperSpawn] && [info exists rt::doParallel] && $rt::doParallel} { 
+     $rt::db killSynthHelper $hsKey
+  } 
+  rt::set_parameter helper_shm_key "" 
     if { [ info exists ::env(RT_TMP) ] } {
       if { [info exists ok_to_delete_rt_tmp] && $ok_to_delete_rt_tmp } { 
         file delete -force $::env(RT_TMP)
