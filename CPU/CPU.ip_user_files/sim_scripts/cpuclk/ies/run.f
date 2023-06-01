f7f3993491fed177b209488d22e5ffee07b83bf0
zhuyucheng30428 <65834966+zhuyucheng30428@users.noreply.github.com>
2023-06-01 19:57:47 +0800

    Delete CPU directory

--- CPU/CPU.ip_user_files/sim_scripts/cpuclk/ies/run.f
+++
@@ -1,8 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../../cpuclk/cpuclk_clk_wiz.v" \
-  "../../../../../cpuclk/cpuclk.v" \
--endlib
--makelib ies_lib/xil_defaultlib \
-  glbl.v
--endlib
-
